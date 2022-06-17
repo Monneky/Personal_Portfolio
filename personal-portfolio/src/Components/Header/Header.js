47c196b6229c96f5f4d77e80ebed11618857c305
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-16 23:14:53 -0500

    add gitignore to backend with flask

--- personal-portfolio/src/Components/Header/Header.js
+++ personal-portfolio/src/Components/Header/Header.js
@@ -1,2 +1,11 @@
 import React from 'react';
 
+function Header() {
+    return (
+        <div>
+            <h1>Testing the header component</h1>
+        </div>
+    );
+}
+
+export default Header;
