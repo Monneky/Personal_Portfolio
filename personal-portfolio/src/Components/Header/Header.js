bc75f273d4f12317675ab7bc0a9d1694d0b1a682
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-17 10:17:42 -0500

    Modified the CSS, create the header and add alt to the ivann logo

--- personal-portfolio/src/Components/Header/Header.js
+++ personal-portfolio/src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LogoIvann from '../../assets/LogoIvann.png'
 
 function Header() {
     return (
         <div>
             <h1>Testing the header component</h1>
+            <img src={LogoIvann} alt="Ivann's Logo"/>
         </div>
     );
 }
