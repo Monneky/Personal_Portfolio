c438756118e713689edba4e7033122da0ce9b209
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-18 17:40:35 -0500

    we add some content more exactly a header only to be sure about the routing. We add the route for all pages

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -4,15 +4,21 @@ import {
     Route,
     Routes 
 } from 'react-router-dom';
-import HomePage from './Pages/Home Page/HomePage'
-
+import HomePage from './Pages/Home Page/HomePage';
+import AboutMe from './Pages/AboutMe/AboutMe';
+import Blog from './Pages/Blog/Blog';
+import Experience from './Pages/Experience/Experience';
+import Contact from './Pages/Contact/Contact';
 const App = () => {
     return(
         <BrowserRouter>
             <div>
                 <Routes>
                     <Route path='/' element={ <HomePage /> }/>    
-                    <Route path='/blog' element={<h1>This is my blog</h1>} />         
+                    <Route path='/about_me' element={ <AboutMe/> } />         
+                    <Route path='/blog' element={ <Blog/> } />
+                    <Route path='/experience' element={ <Experience/> } />
+                    <Route path='/contact' element={ <Contact/> }/>
                 </Routes>
             </div>
         </BrowserRouter>
