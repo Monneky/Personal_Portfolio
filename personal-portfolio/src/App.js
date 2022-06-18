b7bb88142fd8e19caaffae04b2a92c89c03dc9cd
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-18 17:00:55 -0500

    little changes in the project

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import Header from './Components/Header/Header';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
-}
+const App = () => {
+    return(
+        <h1>Hola mundo</h1>               
+    );
+};
 
 export default App;
--- personal-portfolio/src/Pages/blog/blog.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Blog = () => {
-    return (
-        <h1>Hey here is the blog</h1>
-    );
-}
-
-export default Blog;
