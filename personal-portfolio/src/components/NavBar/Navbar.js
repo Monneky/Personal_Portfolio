237852937e3a1d9b9679d495ada1a8fd6bcc1b48
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-19 20:54:31 -0500

    we change the background to check the links and files we add the main information in react

--- personal-portfolio/src/components/NavBar/Navbar.js
+++ personal-portfolio/src/components/NavBar/Navbar.js
@@ -1,28 +1,27 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from '../../assets/Logo.png'
-import "./Navbar.css";
     
 const Navbar = () => {
     return (
         <div>
             <ul>
-                <li className="logo-field">
-                    <NavLink to='/'>
+                <li>
+                    <Link to='/' className="Link">
                         <img src={Logo} alt='Cesar Ivann Logo'/>
-                    </NavLink>
-                </li>
-                <li className="Home">
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li className="Blog">
-                    <NavLink to='/blog'>Blog</NavLink>
+                    </Link>
                 </li>
                 <li>
-                    <NavLink to='/experience'>Experience</NavLink>
+                    <Link to='/'>Home</Link>
                 </li>
                 <li>
-                    <NavLink to='/contact'>Contact</NavLink>
+                    <Link to='/blog'>Blog</Link>
+                </li>
+                <li>
+                    <Link to='/experience'>Experience</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
                 </li>
             </ul>
         </div>
