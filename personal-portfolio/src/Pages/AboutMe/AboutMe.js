aa5591680b9953c880b1c1a53b270dcd446c4024
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-23 17:50:12 -0500

    we add icons and photo for each social network

--- personal-portfolio/src/Pages/AboutMe/AboutMe.js
+++ personal-portfolio/src/Pages/AboutMe/AboutMe.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import photo from '../../assets/photo.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faTwitter,
+    faGithubAlt,
+    faInstagram,
+    faLinkedinIn
+} from '@fortawesome/free-brands-svg-icons';
 
 const AboutMe = () => {
     return(
         <div className='about-me'>
             <div className='up-part'>
                 <img src={ photo } alt='Cesar' className='photo-main'/>
-                
+                <FontAwesomeIcon icon={ faTwitter } size="3x"/>
+                <FontAwesomeIcon icon={ faGithubAlt } size="3x"/>
+                <FontAwesomeIcon icon={ faInstagram } size="3x"/>
+                <FontAwesomeIcon icon={ faLinkedinIn } size="3x" />
             </div>
         </div>
     );
