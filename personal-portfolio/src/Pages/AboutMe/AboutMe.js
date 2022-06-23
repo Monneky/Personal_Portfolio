e7cc1bbb1c2fd42319458181e88d7789e0333fc8
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-23 18:22:07 -0500

    External links to social network

--- personal-portfolio/src/Pages/AboutMe/AboutMe.js
+++ personal-portfolio/src/Pages/AboutMe/AboutMe.js
@@ -13,10 +13,18 @@ const AboutMe = () => {
         <div className='about-me'>
             <div className='up-part'>
                 <img src={ photo } alt='Cesar' className='photo-main'/>
-                <FontAwesomeIcon icon={ faTwitter } size="3x"/>
-                <FontAwesomeIcon icon={ faGithubAlt } size="3x"/>
-                <FontAwesomeIcon icon={ faInstagram } size="3x"/>
-                <FontAwesomeIcon icon={ faLinkedinIn } size="3x" />
+                <a href='https://twitter.com/MonekyDFuego'>
+                    <FontAwesomeIcon icon={ faTwitter } size="3x"/>
+                </a>
+                <a href='https://github.com/Monneky'>
+                    <FontAwesomeIcon icon={ faGithubAlt } size="3x"/>
+                </a>
+                <a href='https://www.instagram.com/project_ivann/'>
+                    <FontAwesomeIcon icon={ faInstagram } size="3x"/>
+                </a>
+                <a href='https://www.linkedin.com/in/cesari-llamas/'>
+                    <FontAwesomeIcon icon={ faLinkedinIn } size="3x" />
+                </a>
             </div>
         </div>
     );
